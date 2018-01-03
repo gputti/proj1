895558e4b254e5ca2bacdb1226f2e042f0b8c565
Gopikrishna <gopiputty@gmail.com>
2018-01-02 21:56:44 -0800

    Update doc2vec_v1.py
    This is working model. expects documents at same level folder ./docs/ and model at same level folder ./model/doc2vec.model 
    use -h option to view

--- doc2vec_v1.py
+++ doc2vec_v1.py
@@ -5,26 +5,22 @@
 # 
 ################################################
 
+import argparse
 from os import listdir
 from os.path import isfile, exists, join
 from os import walk
 import gensim
 from gensim.models.doc2vec import LabeledSentence
-from gensim.models.doc2vec import TaggedLineDocument
+#from gensim.models.doc2vec import TaggedLineDocument
 from pprint import pprint  # pretty-printer
 import nltk
 from nltk.tokenize import word_tokenize
 import re
 import random
 from random import shuffle
+import sys
 
 ############ GLOBAL VARIABLES ############ 
-
-docpath="./asciimath/sof/docs/"
-filename = "output.txt"
-
-modeldocpath="./asciimath/sof/model/"
-modelfilename= modeldocpath + "doc2vec.model"
 
 labledDocs = []
 stoplist = nltk.corpus.stopwords.words('english')
@@ -35,14 +31,15 @@ stoplist = nltk.corpus.stopwords.words('english')
 
 def getAllFilesAsList(dirpath):
     files = []
+    print dirpath
     for (dirpath, dirnames, filenames) in walk(docpath):
         for fn in filenames:
             if not fn.startswith('.'):
-                files.append(fn)
+                files.append(dirpath+fn)
     return files    
 
 ## line numbers start from 0
-def getLineFromFile(srcfilename,linenum):    
+def getLineFromFile(srcfilename,linenum):
     linenum = int(linenum)
     with open(srcfilename, "r") as ins:
         index = 0
@@ -58,18 +55,20 @@ def generate_new_text(text):
         return no_punctuation
 
 
+## provide a fully qualified path of file
 def getLabledDocs(f):
     documents = []
-    for index, text in enumerate(open(docpath+f)):
+    #print(">>> file: " + f)
+    for index, text in enumerate(open(f)):
         text = generate_new_text(text)
         words1 = [w for w in word_tokenize(text) if w not in stoplist]
         documents.append( LabeledSentence(words=words1, tags=['%s_%s' %(f,index)]) )
     return documents
 
-def getLineFromLabledDocs(tag):
+def getLineFromLabledDocs(file1, tag):
     tempfilename, index = tag.split("_")
-    #print ("filename: " + filename + ", index: " + index )
-    return getLineFromFile(docpath + tempfilename, index)
+    #print ("filename: " + file1 + ", index: " + index )
+    return getLineFromFile(file1, index)
 
 
 def debugprint(docs):
@@ -81,18 +80,19 @@ def debugprint(docs):
             break
         print ld[1], ' - ', ld[0]
 
-def printSimOutput(sims):
+def printSimOutput(file1, sims):
     indx = len(sims)    
     print 
     print 'Total num of results : ' , indx 
     cntr = 1
     for index, label in sims:
-        print cntr,') ', getLineFromLabledDocs(index)
+        print ">>>", index, label
+        print cntr,') ', getLineFromLabledDocs(file1,index)
         cntr = cntr + 1
         print ""    
 
 
-def findSimilarByTakingInputline():
+def findSimilarByTakingInputline(files):
     new_doc=''
     while True:    # infinite loop
         new_doc = raw_input("\nEnter the question(q to quit): ")
@@ -103,11 +103,11 @@ def findSimilarByTakingInputline():
         print    
         #sims = model.docvecs.most_similar(positive=[model.infer_vector(new_doc.split())], topn=3)
         ## below is giving better results
-        sims = model.docvecs.most_similar(positive=[model.infer_vector(new_doc)], topn=3)        
-        
-        printSimOutput(sims)
+        sims = model.docvecs.most_similar(positive=[model.infer_vector(new_doc)], topn=3)
+        printSimOutput(files[0], sims)
 
-def findSimilarByRandomNumber():
+def findSimilarByRandomNumber(files):
+    print (">> files", files)
     new_doc=''
     modcount = model.docvecs.count
     rand = 0
@@ -116,13 +116,13 @@ def findSimilarByRandomNumber():
         if toquit == "q":
             break
         rand = random.randint(0,modcount)
-        text = getLineFromFile(docpath+ filename, rand)
+        text = getLineFromFile(files[0], rand)
         print ("finding similar doc for: " + text )
         sims = model.docvecs.most_similar([rand], topn=3)
-        printSimOutput(sims)
+        printSimOutput(files[0], sims)
 
 
-def sentences_perm():
+def shudffledocs():
     shuffle(labledDocs)
     return labledDocs
 
@@ -131,6 +131,29 @@ def sentences_perm():
 print("")
 print('started......')
 print 
+
+
+parser = argparse.ArgumentParser(description='Process input arguments.')
+parser.add_argument('-f', default='0', type=int, dest='findby', help='0 - random docvec, 1 - you have to give doc ') 
+parser.add_argument('-d', dest='docpath', type=str, default='./docs/', help='documents directory') 
+parser.add_argument('-m', dest='modelfilename', type=str, default='./model/doc2vec.model', help='model file name') 
+
+
+args = parser.parse_args()
+findby = args.findby
+docpath=args.docpath
+if docpath == None:
+    print "dir loc is NOT provided. So exiting....."
+    print "for help of params use '-h' option."
+    #sys.exit()
+
+
+
+modelfilename=args.modelfilename
+
+print ( "findby: " , findby )
+print ( "docpath: " , docpath )
+print ( "modelfilename: " , modelfilename )
 
 model = None
 if isfile(modelfilename):
@@ -142,10 +165,11 @@ if model:
     print "model is loaded."
     skip = True
 
+files = [];
+
 if skip:
     print ("skipping the building model")
 else:
-
     files = getAllFilesAsList(docpath)
     print("Reading following files:")
     print(files)
@@ -154,27 +178,25 @@ else:
         labledDocs.extend(getLabledDocs(f))
 
     #debugprint(labledDocs)
-
-    model = gensim.models.Doc2Vec(size=50, window=10, min_count=2, workers=8,alpha=0.025, min_alpha=0.025)
+    model = gensim.models.Doc2Vec(dm=0,size=100, window=10, min_count=2, workers=8,alpha=0.025, min_alpha=0.025)
     model.build_vocab(labledDocs)
-
     for epoch in range(20):
-        model.train(sentences_perm(),total_examples=model.corpus_count, epochs = model.iter)
-        model.alpha -= 0.002 # decrease the learning rate
+        shudffledocs()
+        model.train(labledDocs,total_examples=model.corpus_count, epochs = 1)
+        model.alpha -= 0.001 # decrease the learning rate
         model.min_alpha = model.alpha # fix the learning rate, no deca
-        model.train(sentences_perm(),total_examples=model.corpus_count, epochs = model.iter)
 
     model.save(modelfilename)
-
 
 modcount = model.docvecs.count
 print('no of model docs: ', modcount)
 modcount = modcount - 1
 
-
-#findSimilarByTakingInputline()
-
-findSimilarByRandomNumber()
+files = getAllFilesAsList(docpath)
+if findby == 0:
+    findSimilarByRandomNumber(files)
+else:
+    findSimilarByTakingInputline(files)
 
 
 print("")
